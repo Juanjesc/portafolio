acc4e29b094974ac2ee1060bce1853eac806e55a
Juanjee24 <jsc0009@alu.medac.es>
2021-10-09 12:59:56 +0200

    nuevos cambios al formulario del portafolio

--- correo.php
+++ correo.php
@@ -1,15 +1,18 @@
 <?php 
-    $micorreo = 'juan97jesus@hotmail.com';
+if(isset($_POST['submit'])){
+    
+    
+    $micorreo = "juan97jesus@hotmail.com";
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $asunto = $_POST['asunto'];
     $mensaje = $_POST['mensaje'];
-
-    $header = "Correo enviado desde el portafolio - Juan Jesús Sedano Calderón - ";
-
+    $header = "From: " . $email;
     mail($micorreo, $asunto, $mensaje, $header);
     echo "<script>alert('Mensaje enviado correctamente :D')</script>";
+    echo "<h2>Correo enviado</h2>";
     echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
    
 
+}
 ?>
