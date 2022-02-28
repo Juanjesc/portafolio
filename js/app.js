81920778a5c0544c35201cc4b3d2c2ba8def9186
Juanjee24 <jsc0009@alu.medac.es>
2022-02-28 04:33:23 +0100

    formulario validado, animación box-shadow, textos corregidos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 window.onload = function () {
     
-    var boton_datos = document.getElementById('boton_datos');
+    var boton_submit = document.getElementById('boton_submit');
     
-    boton_datos.onmouseover = cambiar_color;
-    boton_datos.onmouseleave = color_defecto;
-    boton_datos.onclick=function (e) {
+    
+    boton_submit.onmouseover = cambiar_color;
+    boton_submit.onmouseleave = color_defecto;
+    boton_submit.onclick=function (e) {
         e.preventDefault();
         validar();
     }
 
     var iconQuestion = document.querySelectorAll(".fa-question");
-    iconQuestion[0].title ="El nombre debe contener mínimo 3 caracteres, no debe contener números, y no debe estar vacío";
+    iconQuestion[0].title ="El nombre debe contener mínimo 3 caracteres, no debe contener números, y no debe estar vacío.";
+    iconQuestion[1].title ="El email debe ser válido conteniendo el carácter '@', el email no puede estar vacío."
+    iconQuestion[3].title ="El mensaje no debe quedar vacío."
+
+  
+
+    
 }
 
 
@@ -35,8 +42,8 @@ window.addEventListener('scroll', ()=>{
     //var flechita = document.querySelector(".fa-arrow-alt-circle-up");
 
     
-    
     if(scroll>500){
+        navegador.className="boxi";
         navegador.style.position ='fixed'
         navegador.style.top = '0';
         navegador.style.display = 'flex';
@@ -48,7 +55,7 @@ window.addEventListener('scroll', ()=>{
         
         navegador.style.transition = '1s ease-in';
         navegador.style.left='0';
-        navegador.style.padding= .8+"rem";
+        navegador.style.padding= .5+"rem";
         navegador.style.boxShadow='0 5px 10px var(--secundario)';
         navegador.style.margin=0;
        
@@ -69,6 +76,7 @@ window.addEventListener('scroll', ()=>{
   
     }
     else{
+        navegador.className="";
         navegador.style.position ='relative'
         navegador.style.display = 'block';
         navegador.style.width = "100%";
@@ -104,48 +112,152 @@ window.addEventListener('scroll', ()=>{
 //validación formulario
 
 function validar() {
-
+    
     var formu = document.forms[0];
+    var widget = document.querySelector(".widget");
     var c_nombre= formu.nombre;
     var c_email= formu.email;
     var c_asunto = formu.asunto;
+    var c_mensaje = formu.mensaje;
+    var info_error = document.createElement("p"); //mensaje en rojo que alertará de un error
+    info_error.className="errorText";
+    var allcampoDiv = document.querySelectorAll(".campo");
+    var text_name_empty = document.createTextNode("Por favor rellene el campo");
 
     c_nombre.className="";
+    c_email.className="";
     var iconQuestion=document.querySelectorAll(".fa-question");
     for(let i = 0; i<iconQuestion.length;i++){
         iconQuestion[i].style.display="none";
     }
+    
+
+    if(widget.getElementsByTagName("p").length>0){
+        
+    
+        for (let i = widget.getElementsByTagName("p").length-1; i >= 0 ; i--) {
+            widget.removeChild(widget.getElementsByTagName("p")[i]);
+        
+        }
+    }
+    //Validar campo nombre
     if (c_nombre.value == ""||c_nombre.value.length<3){
         
         c_nombre.focus();
         c_nombre.classList.toggle("campoError");
-        document.querySelectorAll(".fa-question")[0].style.display="block";
-        c_nombre.addEventListener("change", function(){
-            c_nombre.className="";
+        if (window.innerWidth >800){
+            
+            document.querySelectorAll(".fa-question")[0].style.display="block";
+        }
+        c_nombre.addEventListener("input", function(){
+            if (c_nombre.value.length>=3){
+                c_nombre.className="";
+            }
         })
 
         if(c_nombre.value == ""){
-            alert("ei pon un nombre crack ");
+            info_error.appendChild(text_name_empty);
+            
+            
         }
         else if(c_nombre.value.length<4){
-            alert("El nombre debe tener al menos 3 caracteres");
+            var text_name_length = document.createTextNode("El nombre debe contener más de 3 letras");
+            info_error.appendChild(text_name_length);
+           
+           
         }   
+        allcampoDiv[0].before(info_error);
+        allcampoDiv[0].addEventListener("change", function(){
+            info_error.style.display="none";
+        })
         return;
     }
     else{
         c_nombre.className="";  
     }
-    if (c_email.value == ""){
-        alert("ei pon un correo crack");
+    // //Validar campo email
+    if (c_email.value == "" || !validarCorreo(c_email.value)){
+
         c_email.focus();
         c_email.classList.toggle("campoError");
+        if (window.innerWidth >800){
+            
+            document.querySelectorAll(".fa-question")[1].style.display="block";
+        }
         
+        c_email.addEventListener("input", function(){
+            if (c_email.value.includes('@')){
 
+                c_email.className="";
+            }
+        })
+
+        if (c_email.value == ""){
+            info_error.appendChild(text_name_empty);
+
+        }
+        else if(!validarCorreo(c_email.value)){
+            var text_email_error = document.createTextNode("Asegúrese de que el correo es válido");
+            info_error.appendChild(text_email_error);
+        }
+       
+        allcampoDiv[1].before(info_error);
+        allcampoDiv[1].addEventListener("change", function(){
+            info_error.style.display="none";
+        })
         return;
 
     }
     else{
-        c_email.classList.toggle("");  
+        c_email.className="";  
+    }
+
+    //Validar campo mensaje (el campo asunto puede estar vacío)
+
+    if (c_mensaje.value == ""){
+
+        info_error.appendChild(text_name_empty);
+        c_mensaje.focus();
+        c_mensaje.classList.toggle("campoError");
+        if (window.innerWidth >800){
+            
+            document.querySelectorAll(".fa-question")[3].style.display="block";
+        }
+        
+        c_mensaje.addEventListener("input", function(){
+            c_mensaje.className="mensajito";
+            
+            
+        });
+        allcampoDiv[3].before(info_error);
+        allcampoDiv[3].addEventListener("input", function(){
+            if (c_mensaje != ""){
+
+                info_error.style.display="none";
+            }
+        })
+
+        return
+
+
+    }
+    else{
+        c_mensaje.className="mensajito";
+    }
+
+
+
+
+    //formu.submit();
+}
+function validarCorreo(correo) {
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var valido = expReg.test(correo);
+    if (valido){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
