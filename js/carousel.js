66c456ca97b9ab992e210a6773d968cb0c726ae5
juanjesc <juan97jesus@hotmail.com>
2022-04-06 21:32:07 +0200

    arreglado bug que impedía cargar más de 2 archivos .js en la cabecera de index.html

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,29 @@
-window.onload = function () {
-    $(function () {
+$(function () {
 
-        $('.owl-carousel').owlCarousel({
-            
-            loop: true,
-            nav: true,
-            navText: ['<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-right"></i>'],
-            dots: true,
-            autoplay:true,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                   items: 1, 
-                   margin: 10,
-                   dotsEach: 2
-                }, 
-                768:{
-                    items:1,
-                    margin: 10
-                   
-                },
-                1500: {
-                    items: 3,
-                    margin: 1
-                }
-                    
-            }
-            
-        });
+    $('.owl-carousel').owlCarousel({
+        
+        loop: true,
+        nav: true,
+        navText: ['<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-right"></i>'],
+        dots: true,
+        autoplay:true,
+        autoplayHoverPause: true,
+        dotsEach: 3,
+        responsive: {
+            0: {
+                items: 1, 
+                margin: 10,
+                dotsEach: 2
+            }, 
+            768:{
+                items:2,
+                margin: 10
+                
+            },
+            1500: {
+                items: 3,
+                margin: 1
+            }  
+        }
     });
-}
+});
